a55af6e15b1ae16f627f7f82361f782d041e94fc
Yong Wei <yongwei996@gmail.com>
2020-05-06 21:45:51 +0800

    list that can strike off

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
+import Todos from './components/Todos';
 
-function App() {
+class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title:"Take out the trash",
+        completed: false
+      },
+      {
+        id: 2,
+        title:"Dinner with wife",
+        completed: true
+      },
+      {
+        id: 3,
+        title:"Meeting with boss",
+        completed: false
+      }
+    ]
+  }
+
+render(){
+  
+  
+
   return (
     <div className="App">
       <h1>App</h1>
+      <Todos todos={this.state.todos}/>
     </div>
   );
+}
 }
 
 export default App;
